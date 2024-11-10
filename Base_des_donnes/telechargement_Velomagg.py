a3e18adc02cc16095cc80c545087c78c97647d29
coralieromani <coralie.romani-de-vinci@etu.umontpellier.fr>
2024-11-10 13:01:08 +0100

    Données restreints à 2023

--- Base_des_donnes/telechargement_Velomagg.py
+++ Base_des_donnes/telechargement_Velomagg.py
@@ -9,11 +9,13 @@ palette = sns.color_palette("twilight", n_colors=12)
 pd.options.display.max_rows = 8
 
 url ="https://data.montpellier3m.fr/sites/default/files/ressources/TAM_MMM_CoursesVelomagg.csv"
+
 path_target = "./CoursesVelomagg.csv"
+
 path, fname = os.path.split(path_target)
 pooch.retrieve(url, path=path, fname=fname, known_hash=None)
 
-Velomagg = pd.read_csv("CoursesVelomagg.csv")
+Velomagg = pd.read_csv(path_target)
 
 # Colonnes souhaitées
 columns =['Departure station', 'Departure', 'Return station', 'Return', 'Duration (sec.)', 'Covered distance (m)']
@@ -23,5 +25,9 @@ Velomagg_filtree = Velomagg[columns]
 
 Velomagg_final = Velomagg_filtree.dropna(subset=['Return station', 'Return'])
 
-Velomagg_final.info()
+Velomagg_2023 = Velomagg_final[Velomagg_final['Departure'].astype(str).str.startswith('2023')]
+
+# Créer un nouveau fichier CSV avec les données de Velomagg_2023
+Velomagg_2023.to_csv("./Velomagg_2023.csv", index=False)
+
 # %%
