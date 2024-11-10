a4ac7996628650ac83ef16737c8354488266e4be
Rokhaya Mbaye <rokhaya.mbaye@etu.umontpellier.fr>
2024-11-10 13:26:11 +0100

    suppression des caractères spéciaux dans velomagg

--- Base_des_donnes/telechargement_Velomagg.py
+++ Base_des_donnes/telechargement_Velomagg.py
@@ -16,7 +16,14 @@ path, fname = os.path.split(path_target)
 pooch.retrieve(url, path=path, fname=fname, known_hash=None)
 
 Velomagg = pd.read_csv(path_target)
-
+Velomagg['Departure station'] = Velomagg['Departure station'].str.replace('Ã©', 'é')
+Velomagg['Return station'] = Velomagg['Return station'].str.replace('Ã©', 'é')
+Velomagg['Departure station'] = Velomagg['Departure station'].str.replace('Ã¨', 'è')
+Velomagg['Return station'] = Velomagg['Return station'].str.replace('Ã¨', 'è')
+Velomagg['Departure station'] = Velomagg['Departure station'].str.replace('Ã´', 'ô')
+Velomagg['Return station'] = Velomagg['Return station'].str.replace('Ã´', 'ô')
+Velomagg.head(100)
+#%%
 # Colonnes souhaitées
 columns =['Departure station', 'Departure', 'Return station', 'Return', 'Duration (sec.)', 'Covered distance (m)']
 
@@ -29,5 +36,7 @@ Velomagg_2023 = Velomagg_final[Velomagg_final['Departure'].astype(str).str.start
 
 # Créer un nouveau fichier CSV avec les données de Velomagg_2023
 Velomagg_2023.to_csv("./Velomagg_2023.csv", index=False)
+Velomagg_final.head(100)
+Velomagg_2023.head(10)
 
 # %%
