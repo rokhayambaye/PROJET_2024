7c0f50c77eb9a61ed0e55580610bad784a9371f4
Rokhaya Mbaye <rokhaya.mbaye@etu.umontpellier.fr>
2024-11-10 10:19:19 +0100

    modif programme

--- Base_des_donnes/telechargementdonneescompt.py
+++ Base_des_donnes/telechargementdonneescompt.py
@@ -3,10 +3,19 @@
 import subprocess
 import os
 import pandas as pd
-
+import seaborn as sns 
+import pooch
 # Lire le fichier CSV avec pandas
-csv_file = '/Users/rokhayambaye/HAX712X/PROJET_2024/Base_des_donnes/MMM_MMM_GeolocCompteurs.csv' 
-df = pd.read_csv(csv_file)
+sns.set_palette("colorblind")
+palette = sns.color_palette("twilight", n_colors=12)
+pd.options.display.max_rows = 8
+# Définir l'URL
+url = 'https://data.montpellier3m.fr/sites/default/files/ressources/MMM_MMM_GeolocCompteurs.csv'
+path_target = "./MMM_MMM_GeolocCompteurs.csv"
+path, fname = os.path.split(path_target)
+pooch.retrieve(url, path=path, fname=fname, known_hash=None)  
+df = pd.read_csv("MMM_MMM_GeolocCompteurs.csv")
+#csv_file = '/Users/rokhayambaye/HAX712X/PROJET_2024/Base_des_donnes/MMM_MMM_GeolocCompteurs.csv' 
 compteurs = df['N° Série'].tolist()
 output_dir = "data_compteurs"
 os.makedirs(output_dir, exist_ok=True)
