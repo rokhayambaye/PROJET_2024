b8d4e816848d02d7e273de7264ecf4f507eb2d64
coralieromani <coralie.romani-de-vinci@etu.umontpellier.fr>
2024-11-15 16:51:13 +0100

    Ajouts d'écocompteurs test

--- carte/carte_interactive.py
+++ carte/carte_interactive.py
@@ -8,40 +8,9 @@ from datetime import datetime
 from matplotlib import cm
 from matplotlib.colors import Normalize
 
-# Fonction pour obtenir le jour de la semaine en toutes lettres
-def get_weekday_name():
-    days = ["Lundi", "Mardi", "Mercredi", "Jeudi", "Vendredi", "Samedi", "Dimanche"]
-    return days[datetime.now().weekday()]  # Renvoie le nom du jour actuel
-
-# Associer une couleur à chaque jour de la semaine
-def get_day_color(day_name):
-    colors = {
-        "Lundi": "blue",
-        "Mardi": "green",
-        "Mercredi": "orange",
-        "Jeudi": "purple",
-        "Vendredi": "red",
-        "Samedi": "brown",
-        "Dimanche": "pink"
-    }
-    return colors.get(day_name, "black")  # Retourne la couleur par défaut (noir) si le jour n'est pas trouvé
-
 stations_df = pd.read_csv('carte/stations_velomagg.csv')
 stations_coords = stations_df[['Latitude', 'Longitude']].values
 
-# Associer un poids à une intervalle de valeurs d'intensité
-# Liste d'intervalles et leurs poids associés
-poids_par_intervalles = [
-    ((0, 100), 1), 
-    ((100, 200), 2),  
-    ((200, 300), 3) 
-]
-# Fonction pour trouver le poids d'une valeur donnée
-def poids(valeur):
-    for (a, b), poids in poids_par_intervalles:
-        if a <= valeur < b:
-            return poids
-    return 0
 # TEST
 data = {
     "Nom": ["Station A", "Station B", "Station C", "Station D", "Station E"],
@@ -51,6 +20,21 @@ data = {
 }
 # Créer le DataFrame
 compteurs_df = pd.DataFrame(data)
+
+# Intervalles et Poids associés
+poids_par_intervalles = [
+    ((0, 100), 1), 
+    ((100, 200), 2),  
+    ((200, 300), 3) 
+]
+
+def poids(valeur):
+    for (a, b), poids in poids_par_intervalles:
+        if a <= valeur < b:
+            return poids
+    return 0
+
+# Ajout colonne Poids 
 compteurs_df['Poids'] = compteurs_df['Intensité'].apply(poids)
 
 # Normaliser les poids
@@ -59,23 +43,18 @@ norm = Normalize(vmin=min(p[1] for p in poids_par_intervalles),
 
 G = ox.graph_from_place( "Montpellier, France", network_type='bike')
 
-# Trouver les nœuds les plus proches des stations
+# Noeuds les plus proches
 stations_nodes = []
 for id, row in stations_df.iterrows():
     lat, lon = row['Latitude'], row['Longitude']
     node = ox.distance.nearest_nodes(G, X=lon, Y=lat)
-    if node not in G:
-        print(f"Le nœud pour la station {row['Nom']} n'est pas dans le réseau cyclable.")
-    else:
-        stations_nodes.append((node, row['Nom']))  # Ajouter le nom de la station ici
+    stations_nodes.append((node, row['Nom']))
 
 # Extraire tous les chemins entre les stations (par pairs)
 paths = []
 for i in range(len(stations_nodes) - 1):
     start_node = stations_nodes[i][0]
     end_node = stations_nodes[i + 1][0]
-    
-   # Tester s'il y a un chemin entre les stations
     if nx.has_path(G, start_node, end_node):
         try:
             route = nx.shortest_path(G, start_node, end_node, weight='length')
@@ -85,13 +64,10 @@ for i in range(len(stations_nodes) - 1):
     else:
         print(f"No path found between {start_node} and {end_node}")
 
-
 # Obtenir le contour de la ville de Montpellier
-place_name = "Montpellier, France"
-area = ox.geocode_to_gdf(place_name)
+area = ox.geocode_to_gdf("Montpellier, France")
 
 fds_coord = [43.6312537,3.8612405] # Coordonnées de la Faculté des Sciences de Montpellier
-
 
 days_of_week = ["Lundi", "Mardi", "Mercredi", "Jeudi", "Vendredi", "Samedi", "Dimanche"]
 
@@ -100,27 +76,35 @@ for day in days_of_week:
     m = folium.Map(location=[43.6117, 3.8777], zoom_start=13)
     m.get_root().html.add_child(folium.Element(f"<h3 style='position: fixed; top: 10px; left: 10px; background-color: white; padding: 5px;'>Carte du {day}</h3>"))
 
-    # Ajouter un marqueur pour la Faculté des Sciences
+    # Faculté des Sciences
     folium.Marker(
         location=fds_coord,
         popup="Faculté des Sciences",
         icon=Icon(icon="university", color="RED", prefix="fa")
     ).add_to(m)
 
- # Ajouter le contour de la ville avec une bordure noire
+    # Contour de la ville
     folium.GeoJson(
         data=area["geometry"],
         style_function=lambda x: {
             "color": "black",
             "weight": 2,
+            "fillOpacity": 0
         }
     ).add_to(m)
 
-    # Ajouter les stations à la carte avec leurs noms
+    # Stations Velomagg
+    for id, row in stations_df.iterrows():
+        folium.Marker(
+            location=[row['Latitude'], row['Longitude']],
+            popup=row['Nom'],
+            icon=Icon(icon='bicycle', color='black', prefix='fa', icon_size=(15, 15))
+        ).add_to(m)
+
+    # Ecocompteurs
     for id, row in compteurs_df.iterrows():
         color = cm.viridis(norm(row['Poids']))
         hex_color = f"#{int(color[0]*255):02x}{int(color[1]*255):02x}{int(color[2]*255):02x}"
-
         folium.CircleMarker(
             location=[row['Latitude'], row['Longitude']],
             radius=8 + row['Poids'],
@@ -130,17 +114,10 @@ for day in days_of_week:
             fill_opacity=0.7,
             popup=f"{row['Nom']}<br>Poids: {row['Poids']}<br>Intensité: {row['Intensité']}"
         ).add_to(m)
-        lat, lon = row['Latitude'], row['Longitude']
-        station_name = row['Nom']
-        folium.Marker(location=[lat, lon], popup=station_name, icon=Icon(icon='bicycle', color='black', prefix='fa', icon_size=(20, 20))).add_to(m)
-
-    # Tracer les chemins passant par les stations avec des couleurs différentes selon le jour
-    day_color = get_day_color(day)  # Récupère la couleur associée au jour
+        
+    # Chemins
     for route in paths:
         route_coords = [(G.nodes[node]['y'], G.nodes[node]['x']) for node in route]
-        folium.PolyLine(locations=route_coords, color=day_color, weight=4, opacity=0.7).add_to(m)
+        folium.PolyLine(locations=route_coords, color='blue', weight=4, opacity=0.7).add_to(m)
 
-m.save(f"carte/map_montpellier_{day}.html")
-
-
-
+    m.save(f"carte/map_montpellier_{day}.html")
