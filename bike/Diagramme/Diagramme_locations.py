41ad55816b2a9e94d3de09277c8d4bf118cd4491
MRAD_Samy <samy.mrad@etu.umontpellier.fr>
2024-12-08 22:43:59 +0100

    Modif diag location et affichage site web

--- bike/Diagramme/Diagramme_locations.py
+++ bike/Diagramme/Diagramme_locations.py
@@ -8,39 +8,32 @@ velomagg_data = pd.read_csv(url_fichier)
 # Convertir les colonnes de date en format datetime
 velomagg_data['Departure'] = pd.to_datetime(velomagg_data['Departure'])
 
-# Extraire les jours pour regrouper les données
-velomagg_data['Day'] = velomagg_data['Departure'].dt.date
+# Ajouter une colonne pour le mois
+velomagg_data['Month'] = velomagg_data['Departure'].dt.month
+mois = ['Janvier', 'Février', 'Mars', 'Avril', 'Mai', 'Juin', 
+        'Juillet', 'Août', 'Septembre', 'Octobre', 'Novembre', 'Décembre']
 
-# Calculer le nombre de locations par jour
-nb_location = velomagg_data.groupby('Day').size()
+# Calculer le nombre moyen de locations par mois
+locations_par_mois = velomagg_data.groupby('Month').size()
 
-# Créer un graphique interactif
-fig = go.Figure()
+# Créer un graphique interactif pour les locations par mois
+fig_mois = go.Figure()
 
-# Ajouter les locations par jour (courbe orange)
-fig.add_trace(go.Scatter(
-    x=nb_location.index, 
-    y=nb_location.values, 
-    mode='lines+markers',
-    name='Locations par jour',
-    line=dict(color='orange'),
-    marker=dict(size=6),
-    yaxis='y1'
+fig_mois.add_trace(go.Bar(
+    x=mois,
+    y=locations_par_mois,
+    name='Locations par mois',
+    marker_color='orange'
 ))
 
-# Mise en page 
-fig.update_layout(
-    title='Nombre de locations de vélomaag 2023 par jour',
-    xaxis=dict(title='Date'),
-    yaxis=dict(
-        title='Nombre de locations',
-        titlefont=dict(color='orange'),
-        tickfont=dict(color='orange')
-    ),
-    legend=dict(orientation='h', yanchor='bottom', y=1.02, xanchor='right', x=1),
-    hovermode='x unified'
+fig_mois.update_layout(
+    title='Nombre moyen de locations Vélomagg par mois (2023)',
+    xaxis_title='Mois',
+    yaxis_title='Nombre de locations',
+    template='plotly_white',
+    height=500
 )
 
-# Sauvegarder et afficher le graphique
-fig.write_html("docs/Diagramme/Diagramme_locations.html")
-fig.show()
+fig_mois.write_html("docs/Diagramme/Diagramme_locations.html")
+fig_mois.show()
+
